818fc35aa53f8a9d490fb879c726a122bd40e1d1
AdelinW <adelin.vulpe@gmail.com>
2025-07-20 16:42:57 +0300

    hitPipe

--- bird.gd
+++ bird.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+
+var hitPipe = false
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		print("space")
 		position.y -= 175
 	
 func _process(delta: float) -> void:
-	position.y += 5
+	if hitPipe == false: position.y += 5
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("Au")
+	hitPipe = true
