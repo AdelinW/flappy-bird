848dce6f526130d0605a9e59ebb378a93c3d53f2
AdelinW <adelin.vulpe@gmail.com>
2025-07-20 17:25:14 +0300

    StartPosition

--- pipe.gd
+++ pipe.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 var hitBird = false
+var startPosition = position
 func _process(delta: float) -> void:
 	if hitBird == false: position.x -= 10
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	position = Vector2(1000,300)
+	position = startPosition
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
