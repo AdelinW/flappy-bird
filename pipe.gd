6b8a5ee86d6c50adbaedf38dbaf372361af86307
AdelinW <adelin.vulpe@gmail.com>
2025-07-20 17:18:10 +0300

    CollisionPipe

--- pipe.gd
+++ pipe.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-
+var hitBird = false
 func _process(delta: float) -> void:
-	position.x -= 10
+	if hitBird == false: position.x -= 10
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	position.x = 1000
-	position.y = 300
 	position = Vector2(1000,300)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	hitBird = true
