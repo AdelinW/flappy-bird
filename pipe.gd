bedc55e3c35b6193db4557837e2885ca44d35a14
AdelinW <adelin.vulpe@gmail.com>
2025-07-22 13:39:16 +0300

    Final few fixes

--- pipe.gd
+++ pipe.gd
@@ -8,6 +8,7 @@ func _process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	position = startPosition
+	hitBird = false
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
